0b9e121946474c7738185cb91703341d39843847
Rozelin <rozelin.dc@gmail.com>
2022-10-22 14:59:00 +0900

    クリーンアーキテクチャ導入

--- go.mod
+++ go.mod
@@ -4,8 +4,10 @@ go 1.16
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/google/uuid v1.1.2
 	github.com/gorilla/sessions v1.2.1
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/labstack/echo-contrib v0.13.0
 	github.com/labstack/echo/v4 v4.9.1
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa
 )
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"github.com/Rozelin-dc/SystemDesignTodoList/repository"
-	"github.com/Rozelin-dc/SystemDesignTodoList/router"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/sessions"
 	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -12,8 +14,29 @@ func main() {
 
 	repo := repository.NewRepository(db)
 
-	echo := router.NewRouter(repo)
-	if err := echo.Start(":80"); err != nil {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(session.Middleware(sessions.NewCookieStore([]byte("secret"))))
+
+	e.POST("/login", repo.NotImpl)
+
+	apiUser := e.Group("/user")
+	{
+		apiUser.POST("", repo.NotImpl)
+		apiUser.DELETE("", repo.NotImpl)
+		apiUser.PATCH("/:uid", repo.NotImpl)
+		apiUser.GET("/me", repo.NotImpl)
+	}
+
+	apiTask := e.Group("/task")
+	{
+		apiTask.POST("", repo.NotImpl)
+		apiTask.PATCH("/:tid", repo.NotImpl)
+		apiTask.DELETE("/:tid", repo.NotImpl)
+	}
+
+	if err := e.Start(":80"); err != nil {
 		panic(err)
 	}
 }
