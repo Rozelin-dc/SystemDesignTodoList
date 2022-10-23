79bcf6896e3b1358c91463f0352931d7a9efb68b
Rozelin <rozelin.dc@gmail.com>
2022-10-23 12:06:25 +0900

    エイリアスの修正とrouterのimport

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
 const devHost = 'localhost:3050'
 
@@ -8,7 +7,7 @@ const devHost = 'localhost:3050'
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src').replace(/\\/g, '/')
+      '@': './src'
     }
   },
   server: {
