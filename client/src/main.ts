8a43e25341506b9e11b5664a3ec0bb1028cf3a65
Rozelin <rozelin.dc@gmail.com>
2022-10-24 09:43:02 +0900

    install element-plus

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,8 +2,12 @@ import { createApp } from 'vue'
 import './style.scss'
 import App from './App.vue'
 
+import ElementPlus from 'element-plus'
+import ja from 'element-plus/lib/locale/lang/ja.js'
+
 import router from '@/router'
 
 const app = createApp(App)
 app.use(router)
+app.use(ElementPlus, { locale: ja })
 app.mount('#app')
