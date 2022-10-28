73c347c5e969d04b25b1330a931dc8f669c3a0de
Rozelin <rozelin.dc@gmail.com>
2022-10-28 16:23:57 +0900

    バリエーション更新

--- util/validate.go
+++ util/validate.go
@@ -3,10 +3,11 @@ package util
 import (
 	"regexp"
 
+	"github.com/labstack/echo/v4"
 	"gopkg.in/go-playground/validator.v9"
 )
 
-type Validator struct {
+type CustomValidator struct {
 	validator *validator.Validate
 }
 
@@ -14,24 +15,25 @@ var expressions = map[string]*regexp.Regexp{
 	"userName":  regexp.MustCompile(`^[a-zA-Z0-9_-]{1,30}$`),
 	"taskName":  regexp.MustCompile(`^.{1,60}$`),
 	"timeLimit": regexp.MustCompile(`^[1-2][0-9]{3}/[0-1][0-9]/[0-3][0-9]$`),
-	"password":  regexp.MustCompile(`^[\x21-\x7E]{10,32}$`),
+	"password":  regexp.MustCompile(`^[\x21-\x7E]{10,30}$`),
 }
 
-func (v *Validator) Validate(i interface{}) error {
-	return v.validator.Struct(i)
-}
-
-func GetValidator() *Validator {
-	v := validator.New()
-
+func (v *CustomValidator) Validate(i interface{}) error {
 	for key, e := range expressions {
-		v.RegisterValidation(key, func(fl validator.FieldLevel) bool {
-			return e.MatchString(fl.Field().String())
+		re := e
+		v.validator.RegisterValidation(key, func(fl validator.FieldLevel) bool {
+			return re.MatchString(fl.Field().String())
 		})
 	}
-	v.RegisterValidation("status", func(fl validator.FieldLevel) bool {
+	v.validator.RegisterValidation("status", func(fl validator.FieldLevel) bool {
 		return fl.Field().Int() == 0 || fl.Field().Int() == 1
 	})
 
-	return &Validator{v}
+	return v.validator.Struct(i)
+}
+
+func GetValidator() echo.Validator {
+	v := validator.New()
+
+	return &CustomValidator{validator: v}
 }
