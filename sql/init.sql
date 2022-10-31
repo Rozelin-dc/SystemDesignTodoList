11c829b3a4439c85f667780ff97bad36a4e54272
Rozelin <rozelin.dc@gmail.com>
2022-10-31 17:06:30 +0900

    sql微修正

--- sql/init.sql
+++ sql/init.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `user_id` VARCHAR(37) NOT NULL,
   `user_name` VARCHAR(30) NOT NULL,
-  `password` VARCHAR(90) DEFAULT NULL,
+  `password` VARCHAR(90) NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_name` (`user_name`)
 );
